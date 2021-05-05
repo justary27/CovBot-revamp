928e5edf691f0e5b345da6172df3b75690e244a9
Ashish Singh <82609788+Zenfoldium@users.noreply.github.com>
2021-05-06 00:42:21 +0530

    Add files via upload

---
+++ Testing/JS/script.js
@@ -0,0 +1,55 @@
+document.getElementById("link1").onmouseover=function(){
+    console.log("ehhl")
+    document.getElementById("link1info").style.display="flex";
+}
+document.getElementById("link1info").onmouseover=function(){
+    console.log("ehhl")
+    document.getElementById("link1info").style.display="flex";
+}
+document.getElementById("link1").onmouseleave=function(){
+    console.log("ehh3l")
+    document.getElementById("link1info").style.display="none";
+}
+
+
+document.getElementById("link2").onmouseover=function(){
+    console.log("ehhl")
+    document.getElementById("link2info").style.display="flex";
+}
+document.getElementById("link2info").onmouseover=function(){
+    console.log("ehhl")
+    document.getElementById("link2info").style.display="flex";
+}
+document.getElementById("link2").onmouseleave=function(){
+    console.log("ehh3l")
+    document.getElementById("link2info").style.display="none";
+}
+
+
+document.getElementById("link3").onmouseover=function(){
+    console.log("ehhl")
+    document.getElementById("link3info").style.display="flex";
+}
+document.getElementById("link3info").onmouseover=function(){
+    console.log("ehhl")
+    document.getElementById("link3info").style.display="flex";
+}
+document.getElementById("link3").onmouseleave=function(){
+    console.log("ehh3l")
+    document.getElementById("link3info").style.display="none";
+}
+
+
+document.getElementById("link4").onmouseover=function(){
+    console.log("ehhl")
+    document.getElementById("link4info").style.display="flex";
+}
+document.getElementById("link4info").onmouseover=function(){
+    console.log("ehhl")
+    document.getElementById("link4info").style.display="flex";
+}
+document.getElementById("link4").onmouseleave=function(){
+    console.log("ehh3l")
+    document.getElementById("link4info").style.display="none";
+}
+
